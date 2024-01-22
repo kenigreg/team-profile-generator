b09e154424fe677342d31af700a09497bdafee64
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-01-22 19:55:24 +0000

    Completed the Manager class

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(officeNumber) {
+        if (!officeNumber || typeof officeNumber !== "number") { 
+            throw new Error("You are missing the office number.");
+        }
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+
+    getRole() {
+        const employeeRole = "Manager";
+        return employeeRole;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+}
