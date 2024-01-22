dd1d330d6b78b0a7d9c8fe7020e19565bfe3a45a
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-01-22 19:53:06 +0000

    Completed the Employee class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {    
+    constructor(name, id, email) {
+        if (!name || typeof name !== "string") { 
+            throw new Error("You are missing the name.");
+        }
+        if (!id || typeof id !== "number") { 
+            throw new Error("You are missing the number.");
+        }
+        if (!email || !email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) { 
+            throw new Error("Please provide a correct email address.");
+        }
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() { 
+        return this.name;
+    }
+    
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+
+    }
+    getRole() {
+        const employeeRole = "Employee";
+        return employeeRole;
+    }
+}
